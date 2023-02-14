b0ea0f1cde7ce32c01053b3cf8f8e6614a48f70d
Santiago Barrios H <13546107+thomas1889@users.noreply.github.com>
2023-02-14 21:31:15 +0100

    Changes over the CSS and JS files
    The css file contains corrections over the display div, so i will show more content within, also got alined all buttons and removed some borders.
    
    When it comes to the JS file it got included functions like isOperator, performCalculation and processInput, so it will be able to validate whether a multiplication or division is taking place, also, would check if division by 0 is happening  due to the method isFinite, and lastly will send the result back to the screenDisplay div within the HTML.

--- calculator.js
+++ calculator.js
@@ -1,21 +1,48 @@
 const screenDisplay = document.querySelector('.screen')
 const buttons = document.querySelectorAll('button')
 
+
 let calculation = []
 let accumulativeCalculation
 
-function calculate(btn) {
-  const value = btn.textContent
+function processInput(btn) {
+  const value = btn.textContent;
+
   if (value === "CLEAR") {
-    calculation = []
-    screenDisplay.textContent = "0"
+    calculation = [];
+    screenDisplay.textContent = ".";
   } else if (value === "=") {
-    screenDisplay.textContent = eval(accumulativeCalculation)
+    performCalculation();
+  } else if (isOperator(value)) {
+    performCalculation();
+    calculation.push(value);
   } else {
-    calculation.push(value)
-    accumulativeCalculation = calculation.join('')
-    screenDisplay.textContent = accumulativeCalculation
+    calculation.push(value);
+    screenDisplay.textContent = calculation.join("");
   }
 }
 
-buttons.forEach(btn => btn.addEventListener('click', () => calculate(btn)))
+function performCalculation() {
+  let expression = calculation.join("");
+  let result;
+  try {
+    result = eval(expression);
+    if (isFinite(result)) {
+      screenDisplay.textContent = result;
+      calculation = [result];
+    } else {
+      screenDisplay.textContent = "Error: Division by zero";
+      calculation = [];
+    }
+  } catch (e) {
+    screenDisplay.textContent = "Error: Invalid expression";
+    calculation = [];
+  }
+}
+
+function isOperator(value) {
+  return ["+", "-", "*", "/"].includes(value);
+}
+
+
+buttons.forEach(btn => btn.addEventListener('click', () => processInput(btn)))
